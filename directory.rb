bc00ee9b2c3d862ed8aefa38bb716ae9a8bb0e81
eb <lizzy.kayballantine@gmail.com>
2021-07-23 16:47:49 +0100

    Add load from students.csv feature

--- directory.rb
+++ directory.rb
@@ -30,6 +30,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -45,6 +47,10 @@ def process(menu_item)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
   when "9"
     exit 
   else
@@ -57,6 +63,25 @@ def interactive_menu
     print_menu
     process(gets.chomp)
   end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 interactive_menu
